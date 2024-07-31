f4f6b07145899a22e9de0ea15911d865e3080d74
Ronit Gurung <lecrepes45@gmail.com>
2024-07-31 08:50:00 +0100

    move to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export const dynamic = "force-dynamic";
 async function Images() {
   const images = await getMyImages();
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image, index) => (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {[...images, ...images, ...images, ...images].map((image, index) => (
         <div key={image.id + "-" + index} className="flex w-48 flex-col">
           <Link href={`/img/${image.id}`}>
             <Image
